41db0a4ab9a2be662103617e8c66dd1e83b5b7ed
Mwaiko <komomwai2005@gmail.com>
2026-06-20 07:41:43 +0300

    Updated Urls

--- backend/educonnectbackend/urls.py
+++ backend/educonnectbackend/urls.py
@@ -5,7 +5,8 @@ from apps.users.urls import auth_urlpatterns, user_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/resources/', include('resources.urls')),
-    path('api/v1/gamification/', include('apps.gamification.urls')),path('api/v1/groups/', include('groups.urls')),
+    path('api/v1/gamification/', include('apps.gamification.urls')),
+    path('api/v1/groups/', include('groups.urls')),
     path('api/v1/auth/', include((auth_urlpatterns, 'auth'))),
     path('api/v1/users/', include((user_urlpatterns, 'users'))),
 ]
